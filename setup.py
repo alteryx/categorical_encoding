59e05d7ec67bb1c0ef252b1d643bb2233b11154f
alexjwang <alex.wang@featurelabs.com>
2019-08-07 14:31:36 -0400

    fixed repeat work in packages

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ with open(path.join(dirname, 'README.md')) as f:
 setup(
     name='categorical_encoding',
     version='0.0.0',
-    packages=find_packages(),
     description='categorical encoding for featuretools',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
