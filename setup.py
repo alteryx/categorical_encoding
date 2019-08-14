51b6509307b41c9f7d3a6a03a254f459e8860381
alexjwang <alex.wang@featurelabs.com>
2019-08-14 15:04:18 -0400

    update version release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(dirname, 'README.md')) as f:
 
 setup(
     name='categorical_encoding',
-    version='0.2.0',
+    version='0.2.1',
     description='categorical encoding for featuretools',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
