34db68ca744f5f8cd36cead5a089847c7aa4094d
alexjwang <alex.wang@featurelabs.com>
2019-08-23 11:21:14 -0400

    demo

--- categorical_encoding/__init__.py
+++ categorical_encoding/__init__.py
@@ -5,4 +5,4 @@ import categorical_encoding.encoders
 import categorical_encoding.primitives
 import categorical_encoding.tests
 
-__version__ = '0.3.1'
+__version__ = '0.4.0'
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(dirname, 'README.md')) as f:
 
 setup(
     name='categorical_encoding',
-    version='0.3.1',
+    version='1.0.0',
     description='categorical encoding for featuretools',
     author='Feature Labs, Inc.',
     author_email='support@featurelabs.com',
